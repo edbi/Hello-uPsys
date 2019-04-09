95d944284d4f88e5c11ebd32c73eaca90990ed3e
eivindbjo <eivibjor@stud.ntnu.no>
2019-04-09 11:12:38 +0200

    initial version

---
+++ test/main.c
@@ -0,0 +1,18 @@
+/*
+ * test.c
+ *
+ * Created: 09.04.2019 11:09:26
+ * Author : Eivind
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
